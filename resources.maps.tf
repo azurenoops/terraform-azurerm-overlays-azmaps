57de3403cfcdaca3d8fb79aa34a648c6378ebfbc
John Spinella <jrspinella@users.noreply.github.com>
2023-05-02 19:18:20 -0400

    updates to loc

--- resources.maps.tf
+++ resources.maps.tf
@@ -12,7 +12,7 @@ resource "azurerm_maps_account" "maps_account" {
 resource "azurerm_maps_creator" "maps_creator" {
   name            = local.maps_creator_name
   maps_account_id = azurerm_maps_account.maps_account.id
-  location        = var.sku == "G2" ? "eastus" : local.location
+  location        = var.sku == "G2" ? "eastus2" : local.location
   storage_units   = var.storage_units
 
   tags = merge(local.default_tags, var.add_tags)
